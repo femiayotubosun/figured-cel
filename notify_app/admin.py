1b69f616bae3aaa5c437dc12390a671617ce5b3b
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2021-10-04 17:32:52 +0100

    Always run in local environment before deploying

--- notify_app/admin.py
+++ notify_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-from models import BroadcastNotification
+from .models import BroadcastNotification
 admin.site.register(BroadcastNotification)
